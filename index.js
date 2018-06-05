0bc3f82e21c4351df9e3dd5fdcd2b8cd3b06807d
sraul <sudarshan_raul@intuit.com>
2018-06-05 19:19:42 +0530

    Intital commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = new express();
+var port = 3000;
+
+app.listen(port, function(err) {
+    if (typeof(err) == "undefined") {
+        console.log('Application is running on : ' + port + ' port');
+    }
+});
+
+app.use(express.static('/'));
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html', { root: __dirname });
+});
