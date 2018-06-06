51ee4531295cb3c014c7411fbc2ae598db799c40
sraul <sudarshan_raul@intuit.com>
2018-06-06 09:31:12 +0530

    Added users get method

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = new express();
 var port = process.env.PORT || 3000;
 
+var users = [{
+    name : "Babu",
+    Address : "RR Nagar"
+  },{
+    name : "Manju",
+    Address : "Banshankari"
+  }
+}];
+
 app.listen(port, function(err) {
     if (typeof(err) == "undefined") {
         console.log('Application is running on : ' + port + ' port');
@@ -12,4 +21,8 @@ app.use(express.static('/'));
 
 app.get('/', function(req, res) {
     res.sendFile('index.html', { root: __dirname });
+});
+
+app.get('/users', function(res. req) {
+    res.send(JSON.stringify(users));
 });
