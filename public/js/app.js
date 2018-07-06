de47db9b969ce670e636a05395f09485c8ae2e3c
sraul <sudarshan_raul@intuit.com>
2018-07-06 18:47:23 +0530

    Added View

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,29 @@
 (function(appName){
     console.log(window.innerWidth);
-    alert(window.innerWidth);
-}("ExpApp"))
+    var loadController = [];
+    if(window.innerWidth < 480) {
+
+    }
+    var MainControllerView = document.querySelectorAll('[data-view="MainControllerView"]')[0];
+    console.log(MainControllerView);
+    getRequest('/view/addItem.html', function (response) {
+        console.log(response);
+        MainControllerView.innerHTML += response;
+    });
+}("ExpApp"));
+
+function getView(view, model) {
+    getRequest(view, function (response) {
+        return response;
+    });
+}
+
+function getRequest(url, callback) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.responseType = 'text';
+    request.onload = function() {
+        callback(request.response);
+    };
+    request.send();
+}
